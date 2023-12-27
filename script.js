3ff947891272c7a5da5d05f7816edcc30e1a273e
arakhor <csmolenski23@gmail.com>
2023-12-27 22:11:26 +0100

    finish main functionality

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+const inputEl = document.getElementById("text-input");
+const btnEl = document.getElementById("check-btn");
+const resultEl = document.getElementById("result");
+
+btnEl.addEventListener("click", () => {
+  if (!inputEl.value) {
+    alert('Please input a value')
+  } else if (isPalindrome(inputEl.value)) {
+    resultEl.innerHTML = `${inputEl.value} is a palindrome`;
+  } else {
+    resultEl.innerHTML = `${inputEl.value} is not a palindrome`;
+  }
+});
+
 function isPalindrome(input) {
   const inputFiltered = input
     .toLowerCase()
     .split("")
     .filter((ch) => ch.match(/[a-z0-9]/));
-  if (inputFiltered.join("") == inputFiltered.toReversed().join("")) {
-    return true;
-  } else {
-    return false;
-  }
+  return inputFiltered.join("") === inputFiltered.toReversed().join("")
+    ? true
+    : false;
 }
